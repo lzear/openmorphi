59e9ecde466f87d834ecd11e72c7dc109ec7d20b
lzear <antoine.clausse@gmail.com>
2020-04-29 23:10:34 +0200

    Replace apollo by faunadb library to get latest documents from db

--- src/views/SavedAnimations.tsx
+++ src/views/SavedAnimations.tsx
@@ -1,8 +1,9 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import faunadb from 'faunadb';
 import moment from 'moment';
 import { Link } from 'react-router-dom';
 import styled from 'styled-components';
-import { Animation, useGetAnimationsQuery } from '../generated/graphql';
+import { Animation } from '../generated/graphql';
 import Spinner from '../components/Spinner';
 import { AnimationPreview } from './CreateAnimation/FinalRender';
 import SVG from '../components/SVG';
@@ -11,22 +12,49 @@ const Tile = styled.div`
   display: inline-block;
   margin: 10px;
 `;
+const q = faunadb.query;
+const client = new faunadb.Client({
+  secret: process.env.REACT_APP_FAUNADB_SECRET as string,
+});
 
 const SavedAnimations: React.FC<{}> = () => {
-  const { loading, error, data } = useGetAnimationsQuery({
-    variables: { size: 20 },
-  });
-  const aa: (Animation | null)[] | undefined = data?.animations.data;
-  let content = null;
-  if (loading) content = <Spinner />;
-  if (error)
-    content = (
-      <p>An error occurred when trying to load recently saved animations</p>
-    );
-  if (aa)
-    content = aa.map(
-      (a: Animation | null) =>
-        a && (
+  const [anims, setAnims] = useState<Animation[] | null>(null);
+  /**
+   * Unfortunately, I couldn't find an easy way to get the last page using GraphQL
+   */
+  useEffect(() => {
+    const load = async () => {
+      const a: any = await client.query(
+        q.Map(
+          q.Paginate(q.Match(q.Index('animations')), {
+            size: 20,
+            before: null,
+          }),
+          q.Lambda((ref) => q.Get(ref)),
+        ),
+      );
+      if (a.data) {
+        const c = a.data
+          .map(
+            (b: any) =>
+              ({
+                hexcode1: b.data.hexcode1,
+                hexcode2: b.data.hexcode2,
+                html: b.data.html,
+                _ts: b.ts,
+                _id: b.ref.value.id,
+              } as Animation),
+          )
+          .reverse();
+        setAnims(c);
+      }
+    };
+    load();
+  }, []);
+  return (
+    <div>
+      {anims ? (
+        anims.map((a: Animation) => (
           <Tile key={a._id}>
             <Link to={'/animations?id=' + a._id}>
               {moment(a._ts / 1000).format('HH:mm, dd MMM D YYYY')}
@@ -35,9 +63,12 @@ const SavedAnimations: React.FC<{}> = () => {
               <SVG svg={a.html} />
             </AnimationPreview>
           </Tile>
-        ),
-    );
-  return <div>{content}</div>;
+        ))
+      ) : (
+        <Spinner />
+      )}
+    </div>
+  );
 };
 
 export default SavedAnimations;
